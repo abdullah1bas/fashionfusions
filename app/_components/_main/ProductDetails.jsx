57c74922d39cf6d6f18519c002817dbbce7b5133
Abdullah Abazza <7a7abas@gmail.com>
2024-11-09 23:55:32 +0200

    loading lazy

--- app/_components/_main/ProductDetails.jsx
+++ app/_components/_main/ProductDetails.jsx
@@ -37,7 +37,7 @@ const ProductDetails = ({ clickedProduct }) => {
   return (
     <Box className="flex items-center gap-5 flex-col sm:flex-row h-[400px]">
       <Box sx={{ display: "flex", height: "100%",  width: "40%",  [theme.breakpoints.down("sm")]: { width: "100%", }, }}>
-        <Image width={350} height={300} style={{width: "100%"}} src={`${clickedProduct.image}`} alt={clickedProduct.title} />
+        <Image loading="lazy" width={350} height={300} style={{width: "100%"}} src={`${clickedProduct.image}`} alt={clickedProduct.title} />
       </Box>
 
       <Box className="py-4 flex-1 text-center sm:text-left w-full md:w-3/5" >
